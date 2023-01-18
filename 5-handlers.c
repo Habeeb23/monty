47d6f3918048f77df33f7c3fc5c321d61d097075
Habeeb23 <113600396+Habeeb23@users.noreply.github.com>
2023-01-18 15:25:57 +0800

    Update 5-handlers.c

--- 5-handlers.c
+++ 5-handlers.c
@@ -1,69 +1,102 @@
 #include "monty.h"
 
+/**
+ * pchar_handler -  prints the char at the top of the stack,
+ * followed by a new line
+ * @stack: double pointer to the stack
+ * @line_no: the line in which this command is called
+ * Return: nothing
+ */
+void pchar_handler(stack_t **stack, unsigned int line_no)
+{
+	if (!(*stack)->next)
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_no);
+		global.err_status = EXIT_FAILURE;
+		return;
+	}
+	if ((*stack)->next->n < 0 || (*stack)->next->n > 127)
+	{
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_no);
+		global.err_status = EXIT_FAILURE;
+		return;
+	}
+
+	printf("%c\n", (*stack)->next->n);
+}
 
 
+/**
+ * pstr_handler - Prints the string contained in a stack_t linked list.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void pstr_handler(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = (*stack)->next;
 
+	while (tmp && tmp->n != 0 && (tmp->n > 0 && tmp->n <= 127))
+	{
+		printf("%c", tmp->n);
+		tmp = tmp->next;
+	}
 
-global_t global = {EXIT_SUCCESS, NULL};
+	printf("\n");
+
+	(void)line_number;
+}
 
 
 
 /**
- *
- *  * main - starting point for monty interpreter
- *
- *   *
- *
- *    * @argc: arguement count
- *
- *     * @argv: list of arguement passed to the function
- *
- *      *
- *
- *       * Return: EXIT_SUCCESS on success, EXIT_FAILURE otherwise
- *
- *        */
-
-int main(int argc, char *argv[])
-
+ * rotr_handler - Rotates the bottom value of a stack_t linked list to the top.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void rotr_handler(stack_t **stack, unsigned int line_number)
 {
+	stack_t *top, *bottom;
 
-		FILE *monty_fd = NULL;
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+		return;
 
+	top = (*stack)->next;
+	bottom = (*stack)->next;
+	while (bottom->next != NULL)
+		bottom = bottom->next;
 
+	bottom->prev->next = NULL;
+	(*stack)->next = bottom;
+	bottom->prev = *stack;
+	bottom->next = top;
+	top->prev = bottom;
 
-			if (argc != 2)
-
-					{
-
-								fprintf(stderr, "USAGE: monty file\n");
-
-										exit(EXIT_FAILURE);
-
-											}
-
-				monty_fd = fopen(argv[1], "r");
-
-
-
-					if (!monty_fd)
-
-							{
-
-										fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-
-												exit(EXIT_FAILURE);
-
-													}
+	(void)line_number;
+}
 
 
+/**
+ * rotl_handler - Rotates the top value of a stack_t linked list to the bottom.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void rotl_handler(stack_t **stack, unsigned int line_number)
+{
+	stack_t *top, *bottom;
 
-						global.err_status = exec_monty(monty_fd);
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+		return;
 
-							fclose(monty_fd);
+	top = (*stack)->next;
+	bottom = (*stack)->next;
+	while (bottom->next != NULL)
+		bottom = bottom->next;
 
+	top->next->prev = *stack;
+	(*stack)->next = top->next;
+	bottom->next = top;
+	top->next = NULL;
+	top->prev = bottom;
 
-
-								exit(global.err_status);
-
+	(void)line_number;
 }
