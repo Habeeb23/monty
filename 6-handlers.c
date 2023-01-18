835ad6e89f7522f1416895534aa9faede0284a5a
Habeeb23 <113600396+Habeeb23@users.noreply.github.com>
2023-01-18 15:26:31 +0800

    Update 6-handlers.c

--- 6-handlers.c
+++ 6-handlers.c
@@ -1,69 +1,34 @@
-i#include "monty.h"
-
-
-
-
-
-global_t global = {EXIT_SUCCESS, NULL};
-
-
+#include "monty.h"
 
 /**
- *
- *  * main - starting point for monty interpreter
- *
- *   *
- *
- *    * @argc: arguement count
- *
- *     * @argv: list of arguement passed to the function
- *
- *      *
- *
- *       * Return: EXIT_SUCCESS on success, EXIT_FAILURE otherwise
- *
- *        */
-
-int main(int argc, char *argv[])
-
+ * stack_handler - Converts a queue to a stack.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void stack_handler(stack_t **stack, unsigned int line_number)
 {
+	(*stack)->n = STACK;
+	(void)line_number;
+}
 
-		FILE *monty_fd = NULL;
+/**
+ * queue_handler - Converts a stack to a queue.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void queue_handler(stack_t **stack, unsigned int line_number)
+{
+	(*stack)->n = QUEUE;
+	(void)line_number;
+}
 
-
-
-			if (argc != 2)
-
-					{
-
-								fprintf(stderr, "USAGE: monty file\n");
-
-										exit(EXIT_FAILURE);
-
-											}
-
-				monty_fd = fopen(argv[1], "r");
-
-
-
-					if (!monty_fd)
-
-							{
-
-										fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-
-												exit(EXIT_FAILURE);
-
-													}
-
-
-
-						global.err_status = exec_monty(monty_fd);
-
-							fclose(monty_fd);
-
-
-
-								exit(global.err_status);
-
+/**
+ * nop_handler - Does absolutely nothing for the Monty opcode 'nop'.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void nop_handler(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
 }
